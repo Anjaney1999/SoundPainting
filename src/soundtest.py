4d3e31647f56d5faceb85f5b8d77f33522c50302
Messi246 <menonanjaney@outlook.com>
2017-04-02 02:27:04 +0100

    handsmeeting

--- src/soundtest.py
+++ src/soundtest.py
@@ -29,23 +29,63 @@ controller.config.save()
 righthandID = 0
 lefthandID = 0
 while True:
-	previousFrame = controller.frame()
-	time.sleep(0.05)
+	#previousFrame = controller.frame()
+	#time.sleep(0.05)
 	frame = controller.frame()
-	if frame.hands.is_empty or previousFrame.hands.is_empty:
-		continue
-	else:
-		all_hands_now = frame.hands
-		all_hands_start = previousFrame.hands
 
-		if(len(all_hands_now) >= 2) or (len(all_hands_start) >=2):
-			continue
-		else:
-			hand_now = all_hands_now.rightmost
-			hand_before = all_hands_start.rightmost
+  all_hands = frame.hands
+  righthand = all_hands.rightmost
+    
+  pointables = righthand.pointables
+  fingers = righthand.fingers
 
-			vertical_now = hand_now.palm_position.y
-			vertical_before = hand_before.palm_position.y
-			diff = vertical_before - vertical_now
-			if diff > 100:
-				print 'shit'
+  index_finger_list1 = righthand.fingers.finger_type(1)
+  index_finger1 = index_finger_list1[0]
+
+  tip1 = index_finger1.tip_position
+
+  lefthand = all_hands.leftmost
+  pointables = lefthand.pointables
+  fingers = lefthand.fingers
+
+  index_finger_list2 = lefthand.fingers.finger_type(1)
+  index_finger2 = index_finger_list2[0]
+
+  tip2 = index_finger2.tip_position
+
+  thumb_list1 = righthand.fingers.finger_type(1)
+  thumb_1 = thumb_list1[0]
+
+  tipThumb1 = thumb_1.tip_position
+
+  thumb_list2 = lefthand.fingers.finger_type(1)
+  thumb_2 = thumb_list2[0]
+
+  tipThumb2 = thumb_2.tip_position
+
+  if(tip1 == tip2 and tipThumb1 == tipThumb2)
+    print "Meeting"
+
+
+  '''
+  if frame.hands.is_empty or previousFrame.hands.is_empty:
+    continue
+  else:
+    all_hands_now = frame.hands
+    all_hands_start = previousFrame.hands
+
+    if(len(all_hands_now) >= 2) or (len(all_hands_start) >=2):
+      continue
+    else:
+      hand_now = all_hands_now.rightmost
+      hand_before = all_hands_start.rightmost
+
+      vertical_now = hand_now.palm_position.y
+      vertical_before = hand_before.palm_position.y
+      diff = vertical_before - vertical_now
+      if diff > 100:
+        print 'shit'
+  '''       
+    
+      
+			
